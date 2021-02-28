c7c0820d49a5844a563f6e5b37061061e6c14545
Conner Wulf <connerwulf@usf.edu>
2021-02-28 18:17:10 -0500

    started building processes struct

--- Simulator_ConnerWulf.c
+++ Simulator_ConnerWulf.c
@@ -2,6 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+
+
+struct Process
+{
+	int process_id;
+	int eventtype;
+	int arrivalTime;
+	int burstTime;
+};
+
+
+
 /****************************************************************
 *                  Main Body                                    *
 ****************************************************************/
@@ -11,9 +24,23 @@ int main(int argc, char *argv[])
 	int quantum = atoi(argv[1]);
 	int contSwitch = atoi(argv[2]);
 	char * fileName = argv[3];
+	FILE* file = fopen(fileName, "r");
+	char line[16];
+	int numProcesses = 0;
+
+	if(file == NULL)
+	{
+		printf("Error reading in file\n");
+		
+	}
+	while(fgets(line, sizeof(line), file))
+	{
+		numProcesses++;
+	}
 	
-	printf("%d\n", quantum);
-	printf("%d\n", contSwitch);
-	printf("%s\n", fileName);
+	struct Process processes[numProcesses];
 	
+
+	int lineIteration = 0;
+		
 }
