fddf932cfd9aff67f2d3fabe392d914e2d29d138
Conner <connerwulf@usf.edu>
2021-03-09 17:00:04 -0500

    test print state

--- Simulator_ConnerWulf.c
+++ Simulator_ConnerWulf.c
@@ -166,6 +166,13 @@ int main(int argc, char *argv[])
 
 
 	}
+	for(int g = 0; g < numProcesses; g++)
+	{
+		printf("Process %d %d %d %d\n",processes[g].process_id
+						   , processes[g].eventtype
+						   , processes[g].arrivalTime
+					   		, processes[g].burstTime);
+	}
 	//print_queue(head);
 	exit(0);
 
