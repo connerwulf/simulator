d93375ee25a2e3f8607aff3aa4338f677b6ed5f5
Conner <connerwulf@usf.edu>
2021-03-10 14:28:17 -0500

    tetds

--- Simulator_ConnerWulf.c
+++ Simulator_ConnerWulf.c
@@ -164,7 +164,7 @@ int main(int argc, char *argv[])
 			if(time == processes[process_index].arrivalTime && process_index < numProcesses)
 			{
 				//context switch
-				ContextSwitchArrivial(&time, contSwitch, &head, processes[process_index], &printNew, &process_index);
+				ContextSwitchArrivial(&time, contSwitch, &head, *processes[process_index], &printNew, &process_index);
 				// enqueue(&head, &processes[process_index]);
 				// printf("P%d arrives at Time %d \n", processes[process_index].process_id, time);
 				// time = time + contSwitch;
@@ -204,7 +204,7 @@ void ContextSwitchArrivial(int* time, int contSwitch, Node **head, Process *proc
 
 		//context switch
 		enqueue(&head, &processes[process_index]);
-		printf("P%d arrives at Time %d \n", processes[process_index].process_id, time);
+		printf("P%d arrives at Time %d \n", process->process_id, time);
 		*time = *time + contSwitch;
 		*printNew = 1;
 		*process_index++;
